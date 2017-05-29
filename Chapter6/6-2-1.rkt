a45fe1734054da5dcd6674813bf87f83b5507d0f
Zhiqiao Xu <xuzhiqiao97@gmail.com>
2017-05-29 16:44:30 +0800

    Create 6-2-1.rkt

---
+++ Chapter6/6-2-1.rkt
@@ -0,0 +1,5 @@
+(start 300 300);
+(draw-solid-line (make-posn 100 100) (make-posn 50 50) 'red);
+(draw-solid-rect (make-posn 20 10) 50 200 'blue);
+(draw-circle (make-posn 200 10) 50 'red);
+(draw-solid-disk (make-posn 200 10) 50 'green);
